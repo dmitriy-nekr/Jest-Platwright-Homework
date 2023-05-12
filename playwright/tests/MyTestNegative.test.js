a03a7ded218b6218031e25f01ba81c4ba3ad8c54
Nekrasov Dmitriy <dimannekr@mail.ru>
2023-05-13 02:01:50 +0300

    Задание 2 доделано

--- playwright/tests/MyTestNegative.test.js
+++ playwright/tests/MyTestNegative.test.js
@@ -1,18 +1,11 @@
-const { chromium } = require("playwright");
-const { expect } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
-(async () => {
-    const browser = await chromium.launch({
-      headless: false,
-      slowMo: 500,
-      devtools: false
-    });
-    const page = await browser.newPage();
+test("test", async ({ page }) => {
     await page.goto("https://netology.ru");
     await page.click("text=Войти");
     await page.getByPlaceholder('Email').fill("dimannekr@mail.ru");
     await page.getByPlaceholder('Пароль').fill("Password");
     await page.getByTestId('login-submit-btn').click();
     await expect(page.getByTestId('login-error-hint')).toBeVisible();
-    await browser.close();
-  })();
+    
+});
--- playwright/tests/MyTestPositive.test.js
+++ playwright/tests/MyTestPositive.test.js
@@ -1,20 +1,13 @@
-const { chromium } = require("playwright");
-const inputData = require("./user");
-const { expect } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
+const inputData = require("../user");
 
-(async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 500,
-    devtools: false
-  });
-  const page = await browser.newPage();
+test("test", async ({ page }) => {
   await page.goto("https://netology.ru");
   await page.click("text=Войти");
   await page.getByPlaceholder('Email').fill(inputData.login);
   await page.getByPlaceholder('Пароль').fill(inputData.password);
   await page.getByTestId('login-submit-btn').click();
   await expect(page.getByRole('heading', { name: 'Мои курсы и профессии' })).toBeVisible();
-  await browser.close();
-})();
+ 
+});
 
